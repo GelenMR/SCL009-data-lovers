2cfa765488ff879a52256b026f483d1aabe49edb
GelenMR <gelen.montilla.r@gmail.com>
2019-05-02 16:41:36 -0400

    Ordenamiento de codigo, Funcion Filtrar x Nombre

--- src/js/data.js
+++ src/js/data.js
@@ -1,31 +1,45 @@
 /* Manejo de data */
-// FILTRADO POR TIPO intento 1 vita
-window.data = {
-    filterType: (arr, type) => {
-        let pokemonType = [];
-        for (let i = 0; i < arr.length; i++) {
-            if (arr[i].type.length > 1) {
-                if (arr[i].type[0] == type || arr[i].type[1] == type) {
-                    pokemonType.push(arr[i]);
-                }
-        // Hago un nuevo arreglo con los que voy  a identificar
-              
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
-
 const example = () => {
   return 'example';
 };
 
 window.example = example;
 
+// FUNCIÓN FILTRAR POR NOMBRE
+let resultFilterName = [];
+const filterNamePokemon = (data, condition) =>{
+  for (let i= 0; i<data.length; i++){
+    if (data[i].name === condition) {
+    resultFilterName.push(data[i]);
+    }
+  }
+  return resultFilterName;
+};
+console.log(filterNamePokemon(dataPokemon, searchName));
+window.resultFilterName = resultFilterName;
+
+
+
 // FUNCIÓN FILTRAR POR TIPO
-const dataPokemonFire = (dataPokemon.filter (element =>{
-  console.log(element.type.includes("Fire"));
-  return(element.type.includes("Fire"));
-}));
+// const dataPokemonFire = (window.dataPokemon.filter(element =>{
+//   console.log(element.type.includes("Fire"));
+//   return(element.type.includes("Fire"));
+// }));
 
 // FUNCIÓN ORDENAR ASCENDENTE
-const dataPokemonAsc = dataPokemon.sort ((a,b) =>{
-  return (a.name> b.name)
-});
+// const dataPokemonAsc = dataPokemon.sort ((a,b) =>{
+//   return (a.name> b.name)
+// });
+
+// FILTRADO POR TIPO intento 1 VITA
+/*window.data = {
+  filterType: (arr, type) => {
+      let pokemonType = [];
+      for (let i = 0; i < arr.length; i++) {
+          if (arr[i].type.length > 1) {
+              if (arr[i].type[0] == type || arr[i].type[1] == type) {
+                  pokemonType.push(arr[i]);
+              }
+      // Hago un nuevo arreglo con los que voy  a identificar*/
--- src/js/main.js
+++ src/js/main.js
@@ -1,57 +1,66 @@
 
-MUESTRA TARJETITAS EN EL INDEX
-// let data = POKEMON.pokemon;
-// const infoCard = document.getElementById('pokemonId');
+//MUESTRA TARJETITAS EN EL INDEX
+// 
 // let infoCard = '';
-// window.addEventListener("load", function () {
-//   mostrarData(data);
+// let btnCard = document.getElementById("${dataPokemon[i].img}");
+// btnCard.addEventListener("click", () => {
+
+//   showData(dataPokemon[i]);
 // });
-// const mostrarData = (data) => {
-
-
-
+// const showData = (dataPokemon) => {
 //   data.forEach(element => {
-//     infoCard += `<div class="col-md-3">
+//     infoCard = `<div class="col-md-3">
 //         <div class="card">
-//         <img class="card-img-top" src="${element.img}" alt="Card image cap">
+//         <img class="card-img-top" src="${element[i].img}" alt="Card image cap">
 //         <div class="card-body">
-//             <h5 class="card-title">${element.name}</h5>
-//             <p class="card-text">${element.egg}</p>
-//             <p class="card-text">${element.num}</p>
-//             <p class="card-text">${element.weaknesses}</p>
-//             <a href="#" class="btn btn-primary">Ver</a>
+//             <h5 class="card-title">${element[i].name}</h5>
+//             <p class="card-text"> ${element[i].num}</p>
+//             <p class="card-text">${element[i].type}</p>
+//             <p class="card-text">${element[i].egg}</p>
+//             <p class="card-text">${element[i].weaknesses}</p>
 //         </div>
 //         </div>
 //     </div>`
 //   })
-
-//   pokemonId.innerHTML = infoCard
+//   document.getElementById('card').innerHTML = infoCard
 // }
 
 /* Manejo del DOM */
 const dataPokemon = window.POKEMON.pokemon;
 console.log(dataPokemon.length);
-
-const dataPokemonFilterFire = window.dataPokemonFire;
-console.log(dataPokemonFilterFire);
-
-const dataPokemonOrderAs = window.dataPokemonAs;
-console.log(dataPokemonOrderAs);
+window.dataPokemon = dataPokemon;
 
 let showPokemon = '';
 let i;
 //MOSTRANDO POKEMONS EN PANTALLA DENTRO DE UN CONTENEDOR
 for (i = 0; i < dataPokemon.length; i++) {
-        
     showPokemon += `
-        <a href="">
+        <a href="#card" id="${dataPokemon[i].num}">
         <img style="width:110px; height: auto" src= ${dataPokemon[i].img} class="" alt="...">
         </a>`
 }                
 document.getElementById("item").innerHTML = showPokemon;
 
+//FILTRANDO POKEMONS POR NAME
+let btnSearchName = document.getElementById("btnSearchName");
+btnSearchName.addEventListener("click", () => {
+    showPokemon = "";
+    let searchName = document.getElementById("searchName").value;
+    window.filterNamePokemon(dataPokemon, searchName);
+        
+});
+
+document.getElementById("orderAsc").innerHTML = resultFilterName;
+
+
+
+
 //FILTRANDO POKEMONS POR TIPO = Fire
+const dataPokemonFilterFire = window.dataPokemonFire;
+console.log(dataPokemonFilterFire);
 document.getElementById("filter").innerHTML = dataPokemonFilterFire;
 
 //ORDENANDO POKEMONS ALFABETICAMENTE: ASCENDENTE
-document.getElementById("orderAs").innerHTML = dataPokemonOrderAs;
+// const dataPokemonOrderAs = window.dataPokemonAs;
+// console.log(dataPokemonOrderAs);
+// document.getElementById("orderAs").innerHTML = dataPokemonOrderAs;
