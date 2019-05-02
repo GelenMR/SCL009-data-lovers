d1ac7ebde794ee8a00a308dfd15dff672a900206
victmartinez <victmartinez@outlook.com>
2019-05-02 10:51:08 -0400

    cambios sincronizados con gelen

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,30 @@
-let data = POKEMON.pokemon;
-const pokemonId = document.getElementById('pokemonId');
-let infoCard = '';
-window.addEventListener("load", function () {
-  mostrarData(data);
-});
-const mostrarData = (data) => {
+MUESTRA TARJETITAS EN EL INDEX
+// let data = POKEMON.pokemon;
+// const infoCard = document.getElementById('pokemonId');
+// let infoCard = '';
+// window.addEventListener("load", function () {
+//   mostrarData(data);
+// });
+// const mostrarData = (data) => {
 
 
 
-  data.forEach(element => {
-    infoCard += `<div class="col-md-3">
-        <div class="card">
-        <img class="card-img-top" src="${element.img}" alt="Card image cap">
-        <div class="card-body">
-            <h5 class="card-title">${element.name}</h5>
-            <p class="card-text">${element.egg}</p>
-            <p class="card-text">${element.num}</p>
-            <p class="card-text">${element.weaknesses}</p>
-            <a href="#" class="btn btn-primary">Ver</a>
-        </div>
-        </div>
-    </div>`
-  })
+//   data.forEach(element => {
+//     infoCard += `<div class="col-md-3">
+//         <div class="card">
+//         <img class="card-img-top" src="${element.img}" alt="Card image cap">
+//         <div class="card-body">
+//             <h5 class="card-title">${element.name}</h5>
+//             <p class="card-text">${element.egg}</p>
+//             <p class="card-text">${element.num}</p>
+//             <p class="card-text">${element.weaknesses}</p>
+//             <a href="#" class="btn btn-primary">Ver</a>
+//         </div>
+//         </div>
+//     </div>`
+//   })
 
-  pokemonId.innerHTML = infoCard
-}
+//   pokemonId.innerHTML = infoCard
+// }
 
  
