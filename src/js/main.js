bfe5271241b1ff177219b5b97d37fe7f155e9c3e
victmartinez <victmartinez@outlook.com>
2019-05-06 20:16:52 -0400

    TODO ANDANDO TIQUITACA ORDENADO, FILTROS, SEARCH,  Y TEST

--- src/js/main.js
+++ src/js/main.js
@@ -10,7 +10,7 @@ let pokemonCard = '';
 
 //CONTENEDOR DONDE MOSTRAREMOS LA INFO
 const showAll = document.getElementById("root");
-const createCards = (data) => { 
+const createCards = (data) => {
   data.forEach((pokemon) => {
     card = `
     <div class="col-12 col-md-3">
@@ -19,8 +19,8 @@ const createCards = (data) => {
     <img src="${pokemon.img}" /><br>
     <b>Nº </b>${pokemon.num}</b><br>
     <h4>${pokemon.name}</h4>
-    <b>Tipo </b><br> ${pokemon.type+" "}</b><br>
-    <b>Debilidades </b><br> ${pokemon.weaknesses+" "}<br>
+    <b>Tipo </b><br> ${pokemon.type + " "}</b><br>
+    <b>Debilidades </b><br> ${pokemon.weaknesses + " "}<br>
     <b>Huevos </b><br> ${pokemon.egg}</b><br>
     </div>
     </div>
@@ -28,15 +28,15 @@ const createCards = (data) => {
     pokemonCard += card
   })
   showAll.innerHTML = pokemonCard
- }
- // LLAMADO A LA FUNCIÓN QUE CREA LA CARD: EL PARÁMETRO (DATAPOKEMON)
- createCards(dataPokemon);
+}
+// LLAMADO A LA FUNCIÓN QUE CREA LA CARD: EL PARÁMETRO (DATAPOKEMON)
+createCards(dataPokemon);
 
 //  FILTRO POR TIPOS
-filterType.addEventListener("click",(event)=> {
+filterType.addEventListener("click", (event) => {
   pokemonCard = "";
   let condition = event.target.text;
-  createCards(window.filterData.type(dataPokemon,condition));
+  createCards(window.filterData.type(dataPokemon, condition));
 });
 
 // FILTRO POR HUEVOS
@@ -49,35 +49,34 @@ filterEggs.addEventListener("click", (event) => {
 // BUSCAR POKEMONS POR NOMBRE
 btnSearchName.addEventListener("click", () => {
   pokemonCard = "";
-  event.preventDefault(); // corta la pagina para que aparezca la búsqueda
   searchName = document.getElementById("searchName").value;
   //console.log(searchName);
   createCards(window.searchData.name(dataPokemon, searchName));
   //createCards(window.searchData.num(dataPokemon, searchName));       
 });
 
-// // ORDENAR POR NOMBRE ASCENDENTE
-// order.addEventListener("click",(event) => {
-//   pokemonCard = "";
-  
-//   let orderSelect = event.target.text;
-//   let resultOrder = [];
-//   if (orderSelect === "Nombre: A-Z"){
-//     resultOrder.push(sortData(dataPokemon, "name", "asc"));
-//     //console.log(resultOrder); 
-//   }
-//   if (orderSelect === "Nombre: Z-A"){
-//     resultOrder.push(sortData(dataPokemon, "name", "desc"));
-//     //console.log(resultOrder);
-//   }
-//   if (orderSelect === "Número: Ascendente"){
-//     resultOrder.push(sortData(dataPokemon, "num", "numasc"));
-//     //console.log(resultOrder);
-//   }
-//   if (orderSelect === "Número: Descendente"){
-//     resultOrder.push(sortData(dataPokemon, "num", "numdesc"));
-//     //console.log(resultOrder);
-//   }
-//   createCards (dataPokemon);
-// });
+// ORDENAR POR NOMBRE ASCENDENTE
+order.addEventListener("click",(event) => {
+  pokemonCard = "";
+
+  let orderSelect = event.target.text;
+  let resultOrder = [];
+  if (orderSelect === "Nombre: A-Z"){
+    resultOrder.push(sortData(dataPokemon, "name", "asc"));
+    //console.log(resultOrder); 
+  }
+  if (orderSelect === "Nombre: Z-A"){
+    resultOrder.push(sortData(dataPokemon, "name", "desc"));
+    //console.log(resultOrder);
+  }
+  if (orderSelect === "Número: Ascendente"){
+    resultOrder.push(sortData(dataPokemon, "num", "numasc"));
+    //console.log(resultOrder);
+  }
+  if (orderSelect === "Número: Descendente"){
+    resultOrder.push(sortData(dataPokemon, "num", "numdesc"));
+    //console.log(resultOrder);
+  }
+  createCards (dataPokemon);
+});
 window.dataPokemon = dataPokemon;
